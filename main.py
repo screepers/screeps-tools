a36971c11938d34eb55e560a94b7149e77d93cce
admon84 <admonwest84@gmail.com>
2021-04-18 10:55:05 -0600

    Adding screeps API endpoints

--- main.py
+++ main.py
@@ -2,18 +2,26 @@
 # Note: You don't need to call app.run() when running `dev_appserver.py .` or deploying to App Engine,
 # since the application is embedded within the App Engine WSGI application server.
 
-from flask import Flask, render_template
+import threading
+import time
+from screepsapi import API as ScreepsAPI
+from flask import Flask, render_template, json
+
 app = Flask(__name__)
+
+screeps_api = ScreepsAPI(
+    host='screeps.com',
+    secure=True
+)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """Return a custom 404 error."""
+    """404 Page Not Found"""
     return 'Sorry, nothing to see here.', 404
-
 
 @app.errorhandler(500)
 def application_error(e):
-    """Return a custom 500 error."""
+    """500 Internal Server Error"""
     return 'Sorry, unexpected error: {}'.format(e), 500
 
 @app.route('/')
@@ -21,5 +29,53 @@ def home():
     """Return a friendly HTTP greeting."""
     return render_template("index.html")
 
+@app.route('/api/shards')
+def api_shards():
+    """Get list of shards"""
+    api_res = screeps_api.shard_info()
+    return app.response_class(
+        response=json.dumps(api_res),
+        status=200,
+        mimetype='application/json'
+    )
+
+@app.route('/api/terrain/<shard>/<room>')
+def api_terrain():
+    """Get the terrain details for a room"""
+    api_res = screeps_api.room_terrain(room=room, shard=shard, encoded=True)
+    return app.response_class(
+        response=json.dumps(api_res),
+        status=200,
+        mimetype='application/json'
+    )
+
+@app.route('/api/objects/<shard>/<room>')
+def api_objects():
+    """Get the objects for a room"""
+    api_res = screeps_api.room_objects(room=room, shard=shard)
+    return app.response_class(
+        response=json.dumps(api_res),
+        status=200,
+        mimetype='application/json'
+    )
+
+def start_runner():
+    """Start the Screeps API connection runner"""
+    def start_loop():
+        not_started = True
+        while not_started:
+            try:
+                if screeps_api is not None:
+                    print('Screeps API connected!')
+                    not_started = False
+            except:
+                print('Screeps API not yet connected')
+            time.sleep(2)
+
+    print('Started runner')
+    thread = threading.Thread(target=start_loop)
+    thread.start()
+
 if __name__ == "__main__":
+    start_runner()
     app.run(debug=True)
