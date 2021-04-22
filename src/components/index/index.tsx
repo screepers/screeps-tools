c83c56f1221da5edde4260fe977f3f110572b38e
admon84 <admonwest84@gmail.com>
2021-04-22 00:50:06 -0600

    Starting to organize components

--- src/components/index/index.tsx
+++ src/components/index/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import {Container, Row, Col, Card, Button, CardTitle, CardText} from 'reactstrap';
+import {Container, Row, Col, Card, Button, CardTitle, CardText, Alert} from 'reactstrap';
 import {Link} from 'react-router-dom';
 
 export const Index = () => (
     <Container className="index">
         <Row>
-            <br/>
             <Col sm={6}>
                 <Card body inverse style={{ backgroundColor: '#222', borderColor: '#1e1e1e' }}>
                     <CardTitle>Building Planner</CardTitle>
@@ -19,6 +18,15 @@ export const Index = () => (
                     <CardText>Evaluate the potential of your creeps with the Creep Designer.</CardText>
                     <Link to='/creep-designer'><Button>Design Creep</Button></Link>
                 </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col sm={12}>
+                <br />
+                <Alert color="success">
+                    <h4 className="alert-heading">Beta Notice</h4>
+                    <p>The <Link to='/building-planner'>Building Planner</Link> design was updated and the back end was rewritten in python using <a href="https://github.com/admon84/new-screeps-tools/blob/main/main.py" target="_blank">Flask</a> and <a href="https://github.com/screepers/python-screeps/pull/29/files" target="_blank">ScreepsAPI</a>.</p>
+                </Alert>
             </Col>
         </Row>
     </Container>
--- src/components/router.tsx
+++ src/components/router.tsx
@@ -2,16 +2,16 @@ import * as React  from 'react';
 import {BrowserRouter, Route, withRouter, Switch, NavLink} from 'react-router-dom';
 import {RouteComponentProps} from 'react-router';
 
-import {BuildingPlanner} from './building-planner';
-import {CreepDesigner} from './creep-designer';
-import {Index} from './index';
+import {BuildingPlanner} from './building-planner/building-planner';
+import {CreepDesigner} from './creep-designer/creep-designer';
+import {Index} from './index/index';
 
 class AppRouter extends React.Component<RouteComponentProps<{}>> {
     render() {
         return (
             <div className="screeps-tools">
                 <div className="header">
-                    <NavLink to='/' exact>Home</NavLink>
+                    <NavLink to='/' exact>Beta</NavLink>
                     <NavLink to='/building-planner'>Building Planner</NavLink>
                     <NavLink to='/creep-designer'>Creep Designer</NavLink>
                     <a href="https://github.com/admon84/new-screeps-tools" target="_blank" className="float-right">GitHub</a>
