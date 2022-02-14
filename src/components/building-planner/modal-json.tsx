a8c6a52f582e15fd249bacf68dce9b52fefcc2cc
Admon West <admonwest84@gmail.com>
2022-02-13 19:23:51 -0700

    json output formatting

--- src/components/building-planner/modal-json.tsx
+++ src/components/building-planner/modal-json.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import * as LZString from 'lz-string';
 import * as Constants from '../common/constants';
-import {Input, Modal, ModalHeader, ModalBody} from 'reactstrap';
+import {Col, Label, Input, Modal, ModalHeader, ModalBody, Row} from 'reactstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faFileCode} from '@fortawesome/free-solid-svg-icons';
 
 export class ModalJson extends React.Component<ModalProps> {
     state: Readonly<{
         modal: boolean;
+        format: boolean;
     }>;
 
     constructor(props: any) {
         super(props);
         this.state = {
-            modal: false
+            modal: false,
+            format: true,
         };
     }
 
@@ -23,7 +25,6 @@ export class ModalJson extends React.Component<ModalProps> {
         const parent = this.props.planner;
         let json = {
             name: parent.state.room,
-            world: parent.state.world,
             shard: parent.state.shard,
             rcl: parent.state.rcl,
             buildings: buildings
@@ -31,15 +32,22 @@ export class ModalJson extends React.Component<ModalProps> {
         const keepStructures = Object.keys(Constants.CONTROLLER_STRUCTURES);
 
         Object.keys(parent.state.structures).forEach((structure) => {
-            if (keepStructures.indexOf(structure) > -1 && !json.buildings[structure]) {
+            if (keepStructures.indexOf(structure) > -1 &&
+                parent.state.structures[structure].length > 0 &&
+                !json.buildings[structure]) {
                 json.buildings[structure] = {
                     pos: parent.state.structures[structure]
                 };
             }
         });
 
-        return JSON.stringify(json);
+        return json;
     }
+
+    displayJson = () => this.state.format
+        ? JSON.stringify(this.createJson(), null, 2)
+            .replace(/{\n\s+"x": ([0-9]){1,4},\n\s+"y": ([0-9]){1,4}\n\s+}/g, '{"x":$1,"y":$2}')
+        : JSON.stringify(this.createJson());
 
     import(e: any) {
         let json = JSON.parse(e.target.value);
@@ -47,12 +55,16 @@ export class ModalJson extends React.Component<ModalProps> {
     }
 
     shareableLink() {
-        let string = LZString.compressToEncodedURIComponent(this.createJson());
-        return "/building-planner/?share=" + string;
+        let jsonString = JSON.stringify(this.createJson());
+        return "/building-planner/?share=" + LZString.compressToEncodedURIComponent(jsonString);
     }
 
     toggleModal() {
         this.setState({modal: !this.state.modal});
+    }
+
+    toggleFormatting(e: any) {
+        this.setState({format: e.target.checked});
     }
 
     render() {
@@ -61,11 +73,25 @@ export class ModalJson extends React.Component<ModalProps> {
                 <button className="btn btn-secondary" onClick={() => this.toggleModal()} title="Json Output">
                     <FontAwesomeIcon icon={faFileCode} />
                 </button>
-                <Modal isOpen={this.state.modal} toggle={() => this.toggleModal()} className="import-room">
+                <Modal size="lg" isOpen={this.state.modal} toggle={() => this.toggleModal()} className="import-room">
                     <ModalHeader toggle={() => this.toggleModal()}>Json Output</ModalHeader>
                     <ModalBody>
-                        <Input type="textarea" value={this.createJson()} id="json-data" onChange={(e) => this.import(e)} />
-                        <a href={this.shareableLink()} id="share-link">Share Link</a>
+                        <Row>
+                            <Col xs={12}>
+                                <Input type="textarea" wrap="soft" value={this.displayJson()} id="json-data" onChange={(e) => this.import(e)} />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={6}>
+                                <a href={this.shareableLink()} id="share-link">Share Link</a>
+                            </Col>
+                            <Col xs={6}>
+                                <Label className="format-json">
+                                    <Input type="checkbox" name="format-json" checked={this.state.format} onChange={(e) => this.toggleFormatting(e)} />
+                                    Format Json
+                                </Label>
+                            </Col>
+                        </Row>
                     </ModalBody>
                 </Modal>
             </div>
